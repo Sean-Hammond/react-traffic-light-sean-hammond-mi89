bd971e49a7d7bd5e6eacf3dee2200fe5e1b1cf09
Sean W. Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2026-02-26 19:36:00 +0000

    Change styling

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,43 +5,46 @@ const Home = () => {
 
   return (
     <div id="container">
-      <div
-        className={
-          "trafficLight text-center " +
-          (color === "red"
-            ? "lightGlow bg-danger border border-5 border-danger"
-            : "bg-danger-subtle border-0")
-        }
-        onClick={() => setColor("red")}
-      >
-        {" "}
-        R{" "}
-      </div>
-      <br />
-      <div
-        className={
-          "trafficLight text-center " +
-          (color === "yellow"
-            ? "lightGlow bg-warning border border-5 border-warning"
-            : "bg-warning-subtle border-0")
-        }
-        onClick={() => setColor("yellow")}
-      >
-        {" "}
-        Y{" "}
-      </div>
-      <br />
-      <div
-        className={
-          "trafficLight text-center " +
-          (color === "green"
-            ? "lightGlow bg-success border border-5 border-success"
-            : "bg-success-subtle border-0")
-        }
-        onClick={() => setColor("green")}
-      >
-        {" "}
-        G{" "}
+      {/* <div className="traffic-light-pole"> </div> */}
+	  <div className="traffic-light-container">
+        <div
+          className={
+            "traffic-light text-center " +
+            (color === "red"
+              ? "lightGlow bg-danger border border-5 border-danger"
+              : "bg-danger-subtle border-0")
+          }
+          onClick={() => setColor("red")}
+        >
+          {" "}
+          R{" "}
+        </div>
+        <br />
+        <div
+          className={
+            "traffic-light text-center " +
+            (color === "yellow"
+              ? "lightGlow bg-warning border border-5 border-warning"
+              : "bg-warning-subtle border-0")
+          }
+          onClick={() => setColor("yellow")}
+        >
+          {" "}
+          Y{" "}
+        </div>
+        <br />
+        <div
+          className={
+            "traffic-light text-center " +
+            (color === "green"
+              ? "lightGlow bg-success border border-5 border-success"
+              : "bg-success-subtle border-0")
+          }
+          onClick={() => setColor("green")}
+        >
+          {" "}
+          G{" "}
+        </div>
       </div>
     </div>
   );
