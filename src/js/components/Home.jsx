6e521752f88c0583bd5b02cbf4a6cf97d545fa5d
Sean W. Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2026-02-26 16:15:16 +0000

    Simplified 3 states into 1

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,47 +1,47 @@
 import React, { useState } from "react";
 
 const Home = () => {
-  const [red, setRed] = useState("on");
-  const [yellow, setYellow] = useState("off");
-  const [green, setGreen] = useState("off");
-
-  const glowRed = () => {
-    setRed("on");
-    setYellow("off");
-    setGreen("off");
-  };
-  const glowYellow = () => {
-    setYellow("on");
-    setRed("off");
-    setGreen("off");
-  };
-  const glowGreen = () => {
-    setGreen("on");
-    setYellow("off");
-    setRed("off");
-  };
+  const [color, setColor] = useState("red");
 
   return (
     <div id="container">
       <div
-        className={`trafficLight text-center ${red == "on" ? "lightGlow bg-danger border border-5 border-danger" : "bg-danger-subtle border-0"}`}
-        onClick={glowRed}
+        className={
+          "trafficLight" +
+          "text-center" +
+          (color === "red"
+            ? "lightGlow bg-danger border border-5 border-danger"
+            : "bg-danger-subtle border-0")
+        }
+        onClick={() => setColor("red")}
       >
         {" "}
         R{" "}
       </div>
       <br />
       <div
-        className={`trafficLight text-center ${yellow == "on" ? "lightGlow bg-warning border border-5 border-warning" : "bg-warning-subtle border-0"}`}
-        onClick={glowYellow}
+        className={
+          "trafficLight" +
+          "text-center" +
+          (color === "yellow"
+            ? "lightGlow bg-warning border border-5 border-warning"
+            : "bg-warning-subtle border-0")
+        }
+        onClick={() => setColor("yellow")}
       >
         {" "}
         Y{" "}
       </div>
       <br />
       <div
-        className={`trafficLight text-center ${green == "on" ? "lightGlow bg-success border border-5 border-success" : "bg-success-subtle border-0"}`}
-        onClick={glowGreen}
+        className={
+          "trafficLight" +
+          "text-center" +
+          (color === "green"
+            ? "lightGlow bg-success border border-5 border-success"
+            : "bg-success-subtle border-0")
+        }
+        onClick={() => setColor("green")}
       >
         {" "}
         G{" "}
@@ -51,17 +51,3 @@ const Home = () => {
 };
 
 export default Home;
-
-//I suspect the grader will suggest a simpler way like this:
-/*
-const [red, setRed] = useState("true")
-const [yellow, setYellow] = useState("false")
-const [green, setGreen] = useState("false")
-
-const onlyOneFunctionNotThree = ()=>{
-	setRed(!red);
-	setYellow(!yellow);
-	setGreen(!green);
-	}
-*/
-//Yes, I tried that, but it did not work as expected.
